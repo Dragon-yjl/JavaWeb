2c51878ab831973f979ba5b15b7929c929dd6de4
Dragon-yjl <2286920873@qq.com>
2022-01-11 21:15:50 +0800

    手动配置servlet---表白页面

---
+++ MyFirstServlet/src/com/yjl/DemoServlet.java
@@ -0,0 +1,23 @@
+package com.yjl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "DemoServlet",urlPatterns = "/myFirstServlet")
+public class DemoServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter write = response.getWriter();
+        write.println(" Hello,This is my first servlet!");
+    }
+}
